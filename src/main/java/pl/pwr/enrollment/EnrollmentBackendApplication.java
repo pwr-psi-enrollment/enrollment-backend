199763f779d084fb5e07c45c3505c10a1e1a9966
Maciej Nowicki <237996@student.pwr.edu.pl>
2021-01-23 13:43:04 +0100

    Fixed package name

--- src/main/java/pl/pwr/enrollment/EnrollmentBackendApplication.java
+++ src/main/java/pl/pwr/enrollment/EnrollmentBackendApplication.java
@@ -1,4 +1,4 @@
-package pl.pwr.enrollment.enrollment;
+package pl.pwr.enrollment;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/pl/pwr/enrollment/EnrollmentBackendApplicationTests.java
+++ src/test/java/pl/pwr/enrollment/EnrollmentBackendApplicationTests.java
@@ -1,4 +1,4 @@
-package pl.pwr.enrollment.enrollment;
+package pl.pwr.enrollment;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
